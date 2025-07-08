c7fc6f6485aa4abb246d1b81cf53fd25db97e91b
Isaac Loiola <isaackopi@icloud.com>
2025-07-08 16:24:29 -0300

    Script do banco finalizado!

--- Banco/script_completo.sql
+++ Banco/script_completo.sql
@@ -56,11 +56,36 @@ foreign key(id_livro) references livros(id)
 );
 
 create table controle_estoque(
+id int auto_increment,
 id_livro int,
 movimento enum('entrada', 'saida', 'perda'), 
 quantidade int, 
 data_mov datetime default current_timestamp,
+primary key(id),
 foreign key(id_livro) references livros(id)
+);
+
+create table autores(
+id int auto_increment,
+nome varchar(50),
+data_nascimento datetime,
+pais varchar(30),
+cidade_nascimento varchar(85),
+primary key(id)
+);
+
+create table editoras(
+id int auto_increment,
+nome varchar(50),
+pais varchar(30),
+data_criacao datetime,
+primary key(id)
+);
+
+create table generos(
+	id int auto_increment,
+    genero varchar(30),
+    primary key(id)
 );
 
 create table livros(
@@ -192,4 +217,116 @@ create procedure sp_log_usuario_insert(
 begin
 	insert log_usuarios
     values(0, spid_usuario, default, spatividade);
+end$$
+
+delimiter $$
+create procedure sp_reservas_insert(
+    spid_usuario int,
+	spid_livro int,
+    spdata_devolucao datetime
+)
+begin
+	insert reservas
+    values(0, spid_usuario, spid_livro, default, spdata_devolucao, default);
+end$$
+
+delimiter $$
+create procedure sp_reservas_finalizar(
+	spid int
+)
+begin
+	update reservas
+    set status = 'finalizado'
+    where id = spid;
+end$$
+
+delimiter $$
+create procedure sp_livros_insert(
+    spnome varchar(120),
+    spid_autor int,
+    spid_editora int,
+    spdescricao text,
+    spdimensao varchar(7),
+    spid_genero int,
+    spdata_pub datetime,
+    spimagem blob
+)
+begin
+	insert livros
+    values(0, spnome, spid_autos, spid_editora, spdescricao, spdimensao, spid_genero, spdata_pub, spimagem);
+    select last_insert_id();
+end$$
+
+delimiter $$
+create procedure sp_enderecos_insert(
+	spid_usuario int, 
+	spcep varchar(8),
+	splogradouro varchar(75), 
+	spnumero varchar(6),
+	spcomplemento varchar(20),
+	spbairro varchar(25),
+	spcidade varchar(25),
+	spuf char(2),
+	sptipo varchar(30)
+)
+begin 
+	insert enderecos
+    values(0, spid_usuario, spcep, splogradouro, spnumero, spcomplemento, spbairro, spcidade, spuf, sptipo);
+    select last_insert_id();
+end $$
+
+delimiter $$
+create procedure sp_controle_estoque_insert(
+    spid_livro int,
+    spmovimento enum('entrada', 'saida', 'perda'),
+    spquantidade int
+)
+begin
+	insert controle_estoque
+    values(0, spid_livro, spmovimento, spquantidade, default);
+end$$
+
+delimiter $$
+create procedure sp_avaliacao_livro(
+	spid_livro int,
+    spnivel tinyint(5)
+)
+begin
+	insert avaliacao_livro 
+    values(spid_livro, spnivel);
+end $$
+
+delimiter $$
+create procedure sp_autores_insert(
+	spnome varchar(50),
+	spdata_nascimento datetime,
+	sppais varchar(30),
+	spcidade_nascimento varchar(85)
+)
+begin
+	insert autores
+    values(0, spnome, spdata_nascimento, sppais, spcidade_nascimento);
+    select last_insert_id();
+end$$
+
+delimiter $$
+create procedure sp_editoras_insert(
+	spnome varchar(50),
+	sppais varchar(30),
+	spdata_criacao datetime
+)
+begin
+	insert editoras
+    values(0, spnome, sppais, spdata_criacao);
+    select last_insert_id();
+end$$
+
+delimiter $$
+create procedure sp_generos_insert(
+	spgenero varchar(30)
+)
+begin
+	insert generos
+    values(0, spgenro);
+    select last_insert_id();
 end$$
