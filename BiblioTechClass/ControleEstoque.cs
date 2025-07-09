3df7e2f5d6e1b779749b3db26d1c282a72094437
Isaac Loiola <isaackopi@icloud.com>
2025-07-08 22:25:17 -0300

    Método ObterLisa adicionado a classe ControleEstoque!

--- BiblioTechClass/ControleEstoque.cs
+++ BiblioTechClass/ControleEstoque.cs
@@ -34,5 +34,51 @@ namespace BiblioTechClass
             Quantidade = quantidade;
         }
 
+        /// <summary>
+        /// Método para adicionar livros no estoque 
+        /// </summary>
+        public void Adicionar()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_controle_estoque";
+            cmd.Parameters.AddWithValue("spid_livro", Livro.Id);
+            cmd.Parameters.AddWithValue("spmovimento", Movimento);
+            cmd.Parameters.AddWithValue("spquantidade", Quantidade);
+
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+            cmd.Connection.Close();
+        }
+
+
+        /// <summary>
+        /// Lista todos os movimentos da tabela controle de estoque
+        /// </summary>
+        /// <returns>lista ControleEstoque</returns>
+        public List<ControleEstoque> ObterLista()
+        {
+            List<ControleEstoque> controle = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from sp_controle_estoque";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                controle.Add
+                    (
+                        new
+                        (
+                            dr.GetInt32(0),
+                            Livro.ObterPorId(dr.GetInt32(1)),
+                            dr.GetString(2),
+                            dr.GetInt32(3),
+                            dr.GetDateTime(4)
+                        )
+                    );
+            }
+            dr.Close();
+            cmd.Connection.Close();
+
+            return controle;
+        }
     }
 }
