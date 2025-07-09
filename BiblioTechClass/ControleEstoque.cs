40308179c8b182635ed8cb6acc62af049ec4bfee
Isaac Loiola <isaackopi@icloud.com>
2025-07-08 21:45:19 -0300

    Propriedades e método construtores adicionados a classe ControleEstoque !

--- BiblioTechClass/ControleEstoque.cs
+++ BiblioTechClass/ControleEstoque.cs
@@ -8,5 +8,31 @@ namespace BiblioTechClass
 {
     public class ControleEstoque
     {
+        public int Id { get; set; }
+        public Livro Livro { get; set; }
+        public string Movimento { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataMovimentacao { get; set; }
+
+        public ControleEstoque()
+        {
+
+        }
+        public ControleEstoque(int id, Livro livro, string movimento, int quantidade, DateTime dataMovimentacaoe)
+        {
+            Id = id;
+            Livro = livro;
+            Movimento = movimento;
+            Quantidade = quantidade;
+            DataMovimentacao = dataMovimentacaoe;
+        }
+
+        public ControleEstoque(Livro livro, string movimento, int quantidade)
+        {
+            Livro = livro;
+            Movimento = movimento;
+            Quantidade = quantidade;
+        }
+
     }
 }
