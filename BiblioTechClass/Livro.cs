623bb0b08f1a43f5fea37327ab87acba640a5e20
Isaac Loiola <isaackopi@icloud.com>
2025-07-08 21:35:27 -0300

    Classe AvaliacaoLivro concluida!

--- BiblioTechClass/Livro.cs
+++ BiblioTechClass/Livro.cs
@@ -8,5 +8,14 @@ namespace BiblioTechClass
 {
     public class Livro
     {
+        public int Id { get; set; }
+
+        public static Livro ObterPorId(int id)
+        {
+            Livro livro = new();
+
+
+            return livro;
+        }
     }
 }
