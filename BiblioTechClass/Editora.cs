a93f166e6cd6f004cf91681eb90fabf327a8256f
Isaac Loiola <isaackopi@icloud.com>
2025-07-10 22:56:58 -0300

    Propriedades e métodos construtores adicionados a classe Editora!

--- BiblioTechClass/Editora.cs
+++ BiblioTechClass/Editora.cs
@@ -8,5 +8,31 @@ namespace BiblioTechClass
 {
     public class Editora
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public Editora()
+        {
+            
+        }
+        public Editora(int id, string nome, string pais, DateTime dataCriacao)
+        {
+            Id = id;
+            Nome = nome;
+            Pais = pais;
+            DataCriacao = dataCriacao;
+        }
+
+
+        public Editora(string nome, string pais, DateTime dataCriacao)
+        {
+            Nome = nome;
+            Pais = pais;
+            DataCriacao = dataCriacao;
+        }
+
+
     }
 }
