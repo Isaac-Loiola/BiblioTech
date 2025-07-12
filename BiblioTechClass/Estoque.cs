5249cd5307b053e61bba93346051dc48b8c355cb
Isaac Loiola <isaackopi@icloud.com>
2025-07-11 23:07:02 -0300

    Método ObterPorId e ObterLista adicionados a classe Estoque!

--- BiblioTechClass/Estoque.cs
+++ BiblioTechClass/Estoque.cs
@@ -8,6 +8,72 @@ namespace BiblioTechClass
 {
     public class Estoque
     {
+        public Livro Livro { get; set; }
+        public int Quantidade { get; set; }
+        public int Disponiveis { get; set; }
 
+
+        public Estoque()
+        {
+
+        }
+
+        public Estoque(Livro livro, int quantidade, int disponiveis)
+        {
+            Livro = livro;
+            Quantidade = quantidade;
+            Disponiveis = disponiveis;
+        }
+
+
+        public Estoque ObterPorId(int idLivro)
+        {
+            Estoque estoque = new();
+
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from estoques {idLivro}";
+            var dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                estoque = new
+                    (
+                        Livro.ObterPorId(dr.GetInt32(0)),
+                        dr.GetInt32(1),
+                        dr.GetInt32(2)
+
+                    );
+            }
+            dr.Close();
+            cmd.Connection.Close();
+
+            return estoque;
+        }
+
+        public List<Estoque> ObterLista()
+        {
+            List<Estoque> estoques = new();
+
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from estoques";
+            var dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                estoques.Add
+                    (
+                        new
+                        (
+                                Livro.ObterPorId(dr.GetInt32(0)),
+                                dr.GetInt32(1),
+                                dr.GetInt32(2)
+                        )
+                    );
+            }
+            dr.Close();
+            cmd.Connection.Close();
+
+            return estoques;
+        }
     }
 }
