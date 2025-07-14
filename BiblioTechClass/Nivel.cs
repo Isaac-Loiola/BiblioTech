bb09cfc3988b48c83e113df78b095169770af86d
Isaac Loiola <isaackopi@icloud.com>
2025-07-14 19:00:58 -0300

    Propriedades e métodos construtores adicionados a classe Nivel!

---
+++ BiblioTechClass/Nivel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioTechClass
+{
+    class Nivel
+    { 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Nivel()
+        {
+
+        }
+        public Nivel(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
