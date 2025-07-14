560665abcf99eb2cb246f9e062d9575683b14e28
Isaac Loiola <isaackopi@icloud.com>
2025-07-14 18:49:57 -0300

    Construtores e propriedades adiconadas!

--- BiblioTechClass/Log.cs
+++ BiblioTechClass/Log.cs
@@ -8,5 +8,27 @@ namespace BiblioTechClass
 {
     public class Log
     {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime Periodo { get; set; }
+        public bool Atividade { get; set; }
+        public Log(int id, Usuario usuario, DateTime periodo, bool atividade)
+        {
+            Id = id;
+            Usuario = usuario;
+            Periodo = periodo;
+            Atividade = atividade;
+        }
+        public Log( Usuario usuario, DateTime periodo, bool atividade)
+        {
+            Usuario = usuario;
+            Periodo = periodo;
+            Atividade = atividade;
+        }
+        public Log(Usuario usuario, bool atividade)
+        {
+            Usuario = usuario;
+            Atividade = atividade;
+        }
     }
 }
