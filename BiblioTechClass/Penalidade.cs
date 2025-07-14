ebef86813c73ec1a13f968767b4ed47eba6c65af
Isaac Loiola <isaackopi@icloud.com>
2025-07-14 19:17:32 -0300

    Propriedades e métodos construtores adicionados!

--- BiblioTechClass/Penalidade.cs
+++ BiblioTechClass/Penalidade.cs
@@ -8,5 +8,32 @@ namespace BiblioTechClass
 {
     class Penalidade
     {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public string Tipo { get; set; }
+        public string Status { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public Penalidade(int id, Usuario usuario, string tipo, string status, DateTime dataInicio, DateTime dataFim)
+        {
+            Id = id;
+            Usuario = usuario;
+            Tipo = tipo;
+            Status = status;
+            DataInicio = dataInicio;
+            DataFim = dataFim;
+        }
+
+        public Penalidade(int id,string status,DateTime dataFim)
+        {
+            Id = id;
+            Status = status;
+            DataFim = dataFim;
+        }
+        public Penalidade(Usuario usuario, string tipo)
+        {
+            Usuario = usuario;
+            Tipo = tipo;
+        }
     }
 }
