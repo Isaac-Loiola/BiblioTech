d1a1a6477470c77c0baa7292185ce91fba148268
Isaac Loiola <isaackopi@icloud.com>
2025-07-15 12:12:22 -0300

    Propriedades e métodos construtores adicionados.

--- BiblioTechClass/Reserva.cs
+++ BiblioTechClass/Reserva.cs
@@ -8,6 +8,33 @@ namespace BiblioTechClass
 {
     public class Reserva
     {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Livro Livro { get; set; }
+        public DateTime DataReserva { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public string Status { get; set; }
+        public Reserva()
+        {
 
+        }
+        public Reserva(int id, Usuario usuario, Livro livro, DateTime dataReserva, DateTime dataDevolucao, string status)
+        {
+            Id = id;
+            Usuario = usuario;
+            Livro = livro;
+            DataReserva = dataReserva;
+            DataDevolucao = dataDevolucao;
+            Status = status;
+        }
+        public Reserva( Usuario usuario, Livro livro, DateTime dataReserva, DateTime dataDevolucao, string status)
+        {
+            Usuario = usuario;
+            Livro = livro;
+            DataReserva = dataReserva;
+            DataDevolucao = dataDevolucao;
+            Status = status;
+        }
+        
     }
 }
