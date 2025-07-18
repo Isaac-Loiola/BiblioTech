514ca8dff74958474b16aa356394377aca649aaf
Isaac Loiola <isaackopi@icloud.com>
2025-07-18 20:32:09 -0300

    Design UscLivrosUsuarios concluido!

---
+++ BiblioTechDesk/UscLivrosUsuario.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BiblioTechDesk
+{
+    partial class UscLivrosUsuario
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            btnMeusLivros = new Guna.UI2.WinForms.Guna2Button();
+            btnDevolver = new Guna.UI2.WinForms.Guna2Button();
+            lblVisaoGeral = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            flpMeusLivros = new FlowLayoutPanel();
+            lblNadaPorAqui = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2Panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BorderRadius = 10;
+            guna2Panel1.Controls.Add(btnDevolver);
+            guna2Panel1.Controls.Add(btnMeusLivros);
+            guna2Panel1.CustomizableEdges = customizableEdges11;
+            guna2Panel1.FillColor = Color.FromArgb(247, 247, 247);
+            guna2Panel1.Location = new Point(3, 12);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Panel1.Size = new Size(202, 34);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // btnMeusLivros
+            // 
+            btnMeusLivros.BackColor = Color.Transparent;
+            btnMeusLivros.BorderRadius = 10;
+            btnMeusLivros.CustomizableEdges = customizableEdges9;
+            btnMeusLivros.DisabledState.BorderColor = Color.DarkGray;
+            btnMeusLivros.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnMeusLivros.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnMeusLivros.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnMeusLivros.FillColor = Color.FromArgb(247, 247, 247);
+            btnMeusLivros.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnMeusLivros.ForeColor = Color.Black;
+            btnMeusLivros.Location = new Point(3, 5);
+            btnMeusLivros.Name = "btnMeusLivros";
+            btnMeusLivros.PressedColor = Color.White;
+            btnMeusLivros.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnMeusLivros.Size = new Size(94, 24);
+            btnMeusLivros.TabIndex = 1;
+            btnMeusLivros.Text = "Meus Livros";
+            // 
+            // btnDevolver
+            // 
+            btnDevolver.BackColor = Color.Transparent;
+            btnDevolver.BorderRadius = 10;
+            btnDevolver.CustomizableEdges = customizableEdges7;
+            btnDevolver.DisabledState.BorderColor = Color.DarkGray;
+            btnDevolver.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDevolver.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDevolver.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDevolver.FillColor = Color.FromArgb(247, 247, 247);
+            btnDevolver.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            btnDevolver.ForeColor = Color.Black;
+            btnDevolver.Location = new Point(103, 5);
+            btnDevolver.Name = "btnDevolver";
+            btnDevolver.PressedColor = Color.White;
+            btnDevolver.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnDevolver.Size = new Size(94, 24);
+            btnDevolver.TabIndex = 1;
+            btnDevolver.Text = "A devolver";
+            // 
+            // lblVisaoGeral
+            // 
+            lblVisaoGeral.BackColor = Color.Transparent;
+            lblVisaoGeral.Font = new Font("Segoe UI Semibold", 20F, FontStyle.Bold);
+            lblVisaoGeral.ForeColor = Color.Black;
+            lblVisaoGeral.IsSelectionEnabled = false;
+            lblVisaoGeral.Location = new Point(3, 52);
+            lblVisaoGeral.Name = "lblVisaoGeral";
+            lblVisaoGeral.Size = new Size(140, 39);
+            lblVisaoGeral.TabIndex = 4;
+            lblVisaoGeral.Text = "Visão Geral";
+            // 
+            // flpMeusLivros
+            // 
+            flpMeusLivros.AutoScroll = true;
+            flpMeusLivros.Location = new Point(0, 97);
+            flpMeusLivros.Name = "flpMeusLivros";
+            flpMeusLivros.Size = new Size(1054, 744);
+            flpMeusLivros.TabIndex = 6;
+            // 
+            // lblNadaPorAqui
+            // 
+            lblNadaPorAqui.BackColor = Color.Transparent;
+            lblNadaPorAqui.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            lblNadaPorAqui.ForeColor = Color.Black;
+            lblNadaPorAqui.IsSelectionEnabled = false;
+            lblNadaPorAqui.Location = new Point(428, 408);
+            lblNadaPorAqui.Name = "lblNadaPorAqui";
+            lblNadaPorAqui.Size = new Size(118, 23);
+            lblNadaPorAqui.TabIndex = 7;
+            lblNadaPorAqui.Text = "Nada por aqui...";
+            lblNadaPorAqui.Visible = false;
+            // 
+            // UscLivrosUsuario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(flpMeusLivros);
+            Controls.Add(lblVisaoGeral);
+            Controls.Add(guna2Panel1);
+            Controls.Add(lblNadaPorAqui);
+            Name = "UscLivrosUsuario";
+            Size = new Size(1054, 854);
+            Load += UscLivrosUsuario_Load;
+            guna2Panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button btnMeusLivros;
+        private Guna.UI2.WinForms.Guna2Button btnDevolver;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblVisaoGeral;
+        private FlowLayoutPanel flpMeusLivros;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblNadaPorAqui;
+    }
+}
